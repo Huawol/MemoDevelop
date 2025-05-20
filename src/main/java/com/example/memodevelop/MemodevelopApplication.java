6f09d61e14a46c91d54d5e0468c2b41f1fc5d6fb
taehyeon <max5915@naver.com>
2025-05-20 19:14:36 +0900

    start

---
+++ src/main/java/com/example/memodevelop/MemodevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.memodevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemodevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemodevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/memodevelop/MemodevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memodevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemodevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
